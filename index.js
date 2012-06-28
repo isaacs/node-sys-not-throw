515ca1a5f7ef442aa197f17e85744fd9e4b61de8
Oleg Efimov <efimovov@gmail.com>
2012-06-29 00:27:05 +0400

    Really works now

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ var util = require('util');
 var msg = 'require("sys-not-throw") is a dirty hack. Please use "util" module instead of "sys" and patch your dependencies.';
 
 var sys = {};
-
 sys.print = util.deprecate(util.print, msg);
 sys.puts = util.deprecate(util.puts, msg);
 sys.debug = util.deprecate(util.debug, msg);
@@ -21,4 +20,5 @@ sys.exec = util.deprecate(util.exec, msg);
 sys.pump = util.deprecate(util.pump, msg);
 sys.inherits = util.deprecate(util.inherits, msg);
 
-require("module")._cache.sys = sys;
+require("module")._cache.sys = {};
+require("module")._cache.sys.exports = sys;
